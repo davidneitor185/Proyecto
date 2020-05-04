cb4c36f7df4166868476609e544d482dfbeb6ec6
Santiago <Santiago@DESKTOP-LOSHLU9>
2020-05-04 02:35:38 -0500

    Graficos

--- src/Graficos/Grafico.java
+++ src/Graficos/Grafico.java
@@ -8,8 +8,11 @@ package Graficos;
 import java.util.ArrayList;
 import modelo.Infectado;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 
 
@@ -20,11 +23,12 @@ import org.jfree.data.general.DefaultPieDataset;
 public class Grafico {
 
     public Grafico() {
+     
     }
     
     
     
-    public void tortaEdad(ArrayList<Infectado> list){
+    public ChartPanel rela(ArrayList<Infectado> list){
         
         int rela =0;
         int impor =0;
@@ -38,11 +42,57 @@ public class Grafico {
     }
         DefaultPieDataset data= new DefaultPieDataset();
         data.setValue("Relacionado : "+String.valueOf(rela),rela);
-        data.setValue("Importado : "+String.valueOf(impor),impor);
+        data.setValue("impor : "+String.valueOf(impor),impor);
         
         JFreeChart cha = ChartFactory.createPieChart3D("Distribución por tipo", data,true,true,true);
         
-        
+        ChartPanel cp = new ChartPanel(cha);
+        cp.setBounds(500,40,500,400);
+        cp.setVisible(true);
+        return cp;
     }
+    
+    public ChartPanel edad(ArrayList<Infectado> list){
+        
+        int infancia =0;
+        int adol =0;
+        int junv =0;
+        int adul =0;
+        int persoM =0;
+        for(int i =0;i<list.size();i++){
+            if(list.get(i).getEdad()<=11){
+                infancia++;
+            }
+            else if(list.get(i).getEdad()<=18){
+                adol++;
+            }
+            else if(list.get(i).getEdad()<=26){
+                junv++;
+            }
+            else if(list.get(i).getEdad()<=64){
+                adul++;
+            }
+            else{
+                persoM++;
+                }
+            
+    }
+        DefaultCategoryDataset data= new DefaultCategoryDataset();
+        data.addValue(infancia,"Infancia","0-11 años");
+        data.addValue(adol,"Adolecencia","12 - 18 años");
+        data.addValue(junv,"Juventud","19-26 años");
+        data.addValue(adul,"Adultez","27-64 años");
+        data.addValue(persoM,"Anciano","65+ años");
+        
+        JFreeChart cha = ChartFactory.createBarChart3D("Distribución por Edad","Edad",
+                "Años", data,PlotOrientation.HORIZONTAL,true,true,true);
+        
+        ChartPanel cp = new ChartPanel(cha);
+        cp.setBounds(500,40,500,400);
+        cp.setVisible(true);
+        
+        return cp;
+    }
+
     
 }
