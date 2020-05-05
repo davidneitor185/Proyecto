8a6abd3cd864a383ec5358209a58617eaef95f68
Victor <Victor@equipo>
2020-05-05 11:40:13 -0500

    Realizado el server para el chat y haciendo el cliente y la vista funcion de chat agregada

--- src/Vista/Principal.java
+++ src/Vista/Principal.java
@@ -166,7 +166,7 @@ public class Principal extends javax.swing.JFrame {
 
     private void menuClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuClienteActionPerformed
         String nombre = JOptionPane.showInputDialog(null,"Por favor ingrese su nombre");
-        Cliente cliente = new Cliente (nombre);
+        ChatClient cliente = new ChatClient (nombre);
         int x = (jDesktopPane1.getWidth() / 2) - cliente.getWidth() /2;
         int y = (jDesktopPane1.getHeight() / 2) - cliente.getHeight() /2;
 
